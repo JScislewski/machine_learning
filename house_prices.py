236474609b70e642b86450df97ff9cc14b29bf32
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-05 01:15:27 +0100

    Update house_prices.py
    Choose features

--- house_prices.py
+++ house_prices.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
-# save filepath to variable for easier access
 melbourne_file_path = "input\melbourne-housing-snapshot\melb_data.csv"
-# read the data and store data in DataFrame titled melbourne_data
 melbourne_data = pd.read_csv(melbourne_file_path)
-# print a summary of the data in Melbourne data
-print(melbourne_data.describe())
+melbourne_data = melbourne_data.dropna(axis=0)
+y = melbourne_data.Price
+melbourne_features = ["Rooms", "Bathroom", "Landsize", "Lattitude", "Longtitude"]
+X = melbourne_data[melbourne_features]
+print(X.describe())
+print(X.head())
