eee475a779a4acc703903401d815969d00f7339f
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-05 17:41:48 +0100

    Update house_prices.py
    Add Random forest model

--- house_prices.py
+++ house_prices.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from scipy.sparse.construct import random
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
@@ -22,6 +23,15 @@ melbourne_features = ["Rooms", "Bathroom", "Landsize", "Lattitude", "Longtitude"
 X = melbourne_data[melbourne_features]
 
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(
+    "Random Forest Mean Absolute Error: {}".format(
+        mean_absolute_error(val_y, melb_preds)
+    )
+)
 
 for max_leaf_nodes in [5, 50, 500, 5000]:
     my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
