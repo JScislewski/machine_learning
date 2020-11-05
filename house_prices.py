3848028798a661378260ed68b00a2e44a4ee548e
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-05 17:35:39 +0100

    Update house_prices.py
    Calculate mae in different amout of leafs

--- house_prices.py
+++ house_prices.py
@@ -4,6 +4,15 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return mae
+
+
 melbourne_file_path = "input\melbourne-housing-snapshot\melb_data.csv"
 melbourne_data = pd.read_csv(melbourne_file_path)
 melbourne_data = melbourne_data.dropna(axis=0)
@@ -13,17 +22,7 @@ melbourne_features = ["Rooms", "Bathroom", "Landsize", "Lattitude", "Longtitude"
 X = melbourne_data[melbourne_features]
 
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
-melbourne_model = DecisionTreeRegressor()
-melbourne_model.fit(train_X, train_y)
 
-val_predictions = melbourne_model.predict(val_X)
-
-print(mean_absolute_error(val_y, val_predictions))
-
-
-def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
-    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
-    model.fit(train_X, train_y)
-    preds_val = model.predict(val_X)
-    mae = mean_absolute_error(val_y, preds_val)
-    return mae
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: {}\nMean Absolute Error: {}".format(max_leaf_nodes, my_mae))
