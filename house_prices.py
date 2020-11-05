b960c4e8fba63c9ed5b7bd2f997ebb501fe9d7e7
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-05 03:45:56 +0100

    Update house_prices.py
    Add model

--- house_prices.py
+++ house_prices.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 melbourne_file_path = "input\melbourne-housing-snapshot\melb_data.csv"
 melbourne_data = pd.read_csv(melbourne_file_path)
@@ -6,5 +7,10 @@ melbourne_data = melbourne_data.dropna(axis=0)
 y = melbourne_data.Price
 melbourne_features = ["Rooms", "Bathroom", "Landsize", "Lattitude", "Longtitude"]
 X = melbourne_data[melbourne_features]
-print(X.describe())
+melbourne_model = DecisionTreeRegressor(random_state=1)
+melbourne_model.fit(X, y)
+
+print("Making predictions for the following 5 houses:")
 print(X.head())
+print("The predictions are")
+print(melbourne_model.predict(X.head()))
